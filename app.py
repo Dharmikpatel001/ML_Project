ad08fbfcdb3bc369fad8425b48ef467440ea3fc4
Dharmik Patel <pateldharmik202004@gmail.com>
2026-01-10 16:03:22 +0530

    update

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
-
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score, mean_absolute_error
-
-import pickle
-
 import pickle
 
 # Load trained model
