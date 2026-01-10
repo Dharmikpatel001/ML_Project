000cba3f17f57c8428f71419655bef2e62eaa06e
Dharmik Patel <pateldharmik202004@gmail.com>
2026-01-10 16:17:56 +0530

    update

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-import streamlit as st
+import pandas as pd
 import numpy as np
-import joblib
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import streamlit as st
+import pickle
 # Load trained model
-model = joblib.load("aqi_model.joblib")
+model = pickle.load("aqi_model.joblib")
 
 st.set_page_config(page_title="AQI Predictor", layout="centered")
 
