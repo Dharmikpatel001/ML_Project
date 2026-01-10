b418f00112b1d5ae56bbc678ffaf4c395a5a77c7
Dharmik Patel <pateldharmik202004@gmail.com>
2026-01-10 15:46:34 +0530

    update

--- app.py
+++ app.py
@@ -24,12 +24,9 @@ st.write("Enter air pollutant values to predict AQI")
 pm25 = st.number_input("PM2.5", min_value=0.0)
 pm10 = st.number_input("PM10", min_value=0.0)
 no2 = st.number_input("NO2", min_value=0.0)
-so2 = st.number_input("SO2", min_value=0.0)
 co = st.number_input("CO", min_value=0.0)
-o3 = st.number_input("O3", min_value=0.0)
-
 if st.button("Predict AQI"):
-    input_data = np.array([[pm25, pm10, no2, so2, co, o3]])
+    input_data = np.array([[pm25, pm10, no2, co]])
     prediction = model.predict(input_data)
 
     st.success(f"Predicted AQI: {int(prediction[0])}")
